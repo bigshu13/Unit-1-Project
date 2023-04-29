ae69b6ee6471018c34579679ecad9dffc7247997
bigshu13 <bigshu13@gmail.com>
2023-04-29 18:10:27 -0400

    added div and button

---
+++ app.js
@@ -0,0 +1,26 @@
+//8x8 column row board
+// I need 12 pieces different color to put on each side of the board.
+// I want the pieces to set on the black spaces of the board. 
+// I need the pieces to take out the opposite color pieces when they cross over. 
+// need to put a rule to turn the checkers piece into a king piece when it reaches to the otherside of the board.
+// an img of a king crown should pop up on the piece that makes it to the otherside.
+// change moving rules for king pieces so they can move multiple spaces and move backwards. 
+// make sure normal pieces move one space at a time diagonally.
+// when pieces get taken i want them to move to trash area on side of the board. 
+// need to have play again button when game is over.
+// need to add win condtion when all pieces are taken.
+// need to add "i quit" button when player gives up.
+// need to make sure pieces can jump multiple opponent pieces if needed.
+// make sure pieces cant go to red boxes. 
+
+/*-----Constants----*/
+
+
+/*----State Variables--- */
+
+
+/*---cached elements---- */
+
+
+/*---functions---- */
+
